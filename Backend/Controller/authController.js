cdbc2ca0d9f231ce29377040b60d1b711c9f2f2c
Arpan-Maity032 <arpanmaity032@gmail.com>
2025-12-26 21:50:54 +0530

    add login and logout feature in authController

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -6,22 +6,68 @@ const generateToken = (id) =>{
     return jwt.sign({id},process.env.JWT_ACCESS_SECRET,{expiresIn:'30d'});
 };
 
-exports.loginUser = async (req,res) =>{
-    const {email,password} = req.body;
-    const user = await User.findOne({email});
 
-    if(user && (await user.matchPassword(password))){
-        const token = generateToken(user._id);
+// New user registration
+exports.registerUser = async (req,res) =>{
+    try{
+        const {name,email,password} = req.body;
+        const userExists = await User.findOne({email});
 
-        await redisClient.set(`sess:${user._id}`,token,{EX:2592000});
+        if(userExists){
+            return res.status(400).json({message:'User already exixts'});
+        }
 
-        res.json({
-            _id:user._id,
-            name:user.name,
-            token:token
-        });
+        const user = await User.create({name,email,password});
+
+        if(user){
+            res.status(201).json({
+                _id: user._id,
+                name:user.name,
+                email:user.email,
+                token:generateToken(user._id),
+            });
+        }
+    }catch(error){
+        res.status(500).json({message:'error.message'});
     }
-    else{
-        res.status(401).json({message:'Invalid email or password'});
+};
+
+
+// existing user login
+exports.loginUser = async (req,res) =>{
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+
+        if(user && (await user.matchPassword(password))){
+            const token = generateToken(user._id);
+
+            await redisClient.set(`sess:${user._id}`,token,{EX:2592000});
+
+            res.json({
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                avatar:user.avatar_url,
+                token:token,
+            });
+        }
+        else{
+            res.status(401).json({message:'Invalid email or password'});
+        }
+    }catch(error){
+        res.status(500).json({message:'error.message'});
+    }
+};
+
+
+// user logout
+
+exports.logoutUser = async (req,res) =>{
+    try{
+        await redisClien.del(`sess:${req.user._id}`);
+        res.status(200).json({message:'Successfully logged out'});
+    }catch(error){
+        res.status(500).json({message:'error.message'});
     }
 };
