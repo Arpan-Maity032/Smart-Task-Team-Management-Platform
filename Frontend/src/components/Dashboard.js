c3ea964d2674c69ccd34d0fa48a929431677a476
Arpan-Maity032 <arpanmaity032@gmail.com>
2026-01-18 18:08:37 +0530

    add feature in sidebar

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -3,9 +3,9 @@ import Sidebar from './mainproject/Sidebar';
 
 function Dashboard() {
   return (
-    <>
+    <div style={{margin:0,display:"flex"}}>
     <Sidebar/>
-    </>
+    </div>
   )
 }
 
--- Frontend/src/components/mainproject/Sidebar.js
+++ Frontend/src/components/mainproject/Sidebar.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import "../../styles/mainProjectStyle/sidebar.css";
+import Dashboard from "../Dashboard";
 function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      <div className='feature'>
+        <a href="/Dashboard"> Dashboard</a>
+        <a href="#">Create Project</a>
+        <a href="#">Add Member</a>
+        <a href="#">Assign Project</a>
+        <a href="#">Manage Task</a>
+      </div>
+    </div>
   )
 }
 
