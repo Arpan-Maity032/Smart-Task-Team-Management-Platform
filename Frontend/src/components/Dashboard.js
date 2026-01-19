3a3859b1fc5f0f537bbf2e4e1f8bea9712f400bd
Arpan-Maity032 <arpanmaity032@gmail.com>
2026-01-19 21:42:53 +0530

    working on dashboard ui

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Sidebar from './mainproject/Sidebar';
+import Header from './mainproject/Header';
 
 function Dashboard() {
   return (
-    <div style={{margin:0,display:"flex"}}>
-    <Sidebar/>
+    <div style={{margin:0,position:'relative'}}>
+      <Header/>
+      <div style={{display:"flex"}}>
+      <Sidebar/>
+      </div>
     </div>
   )
 }
--- Frontend/src/components/mainproject/Sidebar.js
+++ Frontend/src/components/mainproject/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../../styles/mainProjectStyle/sidebar.css";
-import Dashboard from "../Dashboard";
+import { IoSettingsOutline } from "react-icons/io5";
+import {Link} from "react-router-dom";
 function Sidebar() {
   return (
     <div className='sidebar'>
@@ -10,6 +11,11 @@ function Sidebar() {
         <a href="#">Add Member</a>
         <a href="#">Assign Project</a>
         <a href="#">Manage Task</a>
+      </div>
+      <div className='side-setting'>
+        <Link to="#" className='setting-css'><IoSettingsOutline />
+        <p>Setting and helps</p>
+        </Link>
       </div>
     </div>
   )
